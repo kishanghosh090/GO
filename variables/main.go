8f446b5cd4c019aef9c1aec7ca54c2e9b0a46f15
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-08 18:12:04 +0530

    consts in go

--- variables/main.go
+++ variables/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const age = 20
+
 func main() {
 	fmt.Println("variables")
 	var user string = "hello from kishan"
@@ -9,22 +11,29 @@ func main() {
 
 	var age = 19
 	if age != 0 {
-		
+
 	}
+
+	myName := "kishan"
+	fmt.Println(myName)
 	for i := 0; i < 11; i++ {
 		fmt.Println(i)
 	}
 	var isLoggedIn bool = true // type inference, the type of isLoggedIn will be bool
 	fmt.Println(user)
 	fmt.Println(isLoggedIn)
-	fmt.Printf("variable is of type: %T \n", user) // %T is used to print the type of the variable
+	fmt.Printf("variable is of type: %T \n", user)       // %T is used to print the type of the variable
 	fmt.Printf("variable is of type: %T \n", isLoggedIn) // %T is used to print the type of the variable
 
-	var smallValue uint8 = 255 // uint8 can hold values from 0 to 255
+	var smallValue uint8 = 255                                                       // uint8 can hold values from 0 to 255
 	fmt.Printf("smallValue is of type: %T and value: %d \n", smallValue, smallValue) // %d is used to print the value of the variable
 
-	var largeValue uint64 = 18446744073709551615 // uint64 can hold values from 0 to 18446744073709551615
+	var largeValue uint64 = 18446744073709551615                                     // uint64 can hold values from 0 to 18446744073709551615
 	fmt.Printf("largeValue is of type: %T and value: %d \n", largeValue, largeValue) // %d is used to print the value of the variable
 
-	
+	var myRoll int
+	myRoll = 12
+
+	fmt.Println(myRoll)
+
 }
