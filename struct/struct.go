cd867008384d4353747a0f527ed0b9d9527b60f1
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-08 21:31:13 +0530

    embeding struct

--- struct/struct.go
+++ struct/struct.go
@@ -7,23 +7,66 @@ import (
 
 // order struct
 
+type customer struct {
+	name  string
+	phone int
+}
+
 type order struct {
 	id        int
 	name      string
 	amount    float32
 	status    string
 	createdAt time.Time // nanosecond precision
+	customer
+}
+
+// reciver type
+func (o *order) changeStatus(status string) {
+	o.status = status
+}
+
+// constructor
+func newOrder(id int, amount float32, status string) *order {
+	// initial setup
+	order := order{
+		id:     id,
+		amount: amount,
+		status: status,
+	}
+	return &order
 }
 
 func main() {
 	// var o1 order =
-	order := order{
-		id:        1,
-		name:      "chai",
-		amount:    20,
-		status:    "PENDING",
-		createdAt: time.Now(),
-	}
+	// order := order{
+	// 	id:        1,
+	// 	name:      "chai",
+	// 	amount:    20,
+	// 	status:    "PENDING",
+	// 	createdAt: time.Now(),
+	// }
 
-	fmt.Println(order, order.createdAt.UTC().Local())
+	// fmt.Println(order, order.createdAt.UTC().Local())
+
+	// one time use structure
+	language := struct {
+		name   string
+		isGood bool
+	}{"golang", true}
+
+	fmt.Println(language)
+
+	// embadding
+	newOrder := order{
+		id:     1,
+		amount: 23,
+		status: "CONFIRM",
+		customer: customer{
+			name:  "kishan",
+			phone: 9635859574,
+		},
+	}
+	fmt.Println(newOrder)
+
 }
