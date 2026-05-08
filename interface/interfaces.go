5325e38ee7018d9f417df1ac1ffa01ed632b0f02
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-08 22:50:24 +0530

    interfaces

---
+++ interface/interfaces.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Payment interface {
+	pay(amount float32)
+}
+
+type payment struct {
+	gateway Payment
+}
+
+// razorpay
+type razorpay struct{}
+
+func (r razorpay) pay(amount float32) {
+	fmt.Println("making payment using raz", amount)
+}
+
+// paypal
+type paypal struct{}
+
+func (r paypal) pay(amount float32) {
+	fmt.Println("making payment using raz", amount)
+}
+
+func main() {
+	raz := razorpay{}
+
+	pay := payment{
+		gateway: raz,
+	}
+	pay.gateway.pay(1000)
+}
