728ea886b55d6abc5c01fc7f3d59a53dc488b3b2
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-09 22:37:37 +0530

    file IO in go lanfg

---
+++ fileIO/fileIO.go
@@ -0,0 +1,49 @@
+package main
+
+func main() {
+	// f, err := os.Open("ex.json")
+
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	panic(err)
+	// }
+	// fileInfo, err := f.Stat()
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(fileInfo.IsDir(), fileInfo.Size())
+
+	// read file
+
+	// f, err := os.Open("ex.json")
+
+	// println()
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// buf := make([]byte, 10)
+
+	// d, err := f.Read(buf)
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// println(buf, d)
+
+	// for i := 0; i < len(buf); i++ {
+	// 	println(string(buf[i]))
+	// }
+
+	// defer f.Close()
+
+	// data, err := os.ReadFile("ex.json")
+
+	// if err != nil {
+
+	// }
+
+	// fmt.Print(string(data))
+}
