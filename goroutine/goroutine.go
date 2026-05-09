7c68edb1ee951a506c9ede024c61a873b81534bd
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-09 19:28:04 +0530

    wait groups

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -2,17 +2,24 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
-func task(id int) {
+func task(id int, w *sync.WaitGroup) {
+	defer w.Done()
 	fmt.Println("doing task", id)
-
 }
 
 func main() {
+	var wg sync.WaitGroup
+
 	for i := 0; i <= 100; i++ {
-		go task(i)
+		wg.Add(1)
+		go task(i, &wg)
 	}
-	time.Sleep(time.Second * 2)
+	// time.Sleep(time.Second * 2)
+
+	// wait groups goroutine
+	wg.Wait()
+
 }
