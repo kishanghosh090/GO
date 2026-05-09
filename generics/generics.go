509d024d44d5569bfab708edf91ab80ac8475a67
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-09 10:43:50 +0530

    generics in go

---
+++ generics/generics.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+// func printSlice(items []int) {
+// 	for _, item := range items {
+// 		fmt.Println(item)
+// 	}
+// }
+
+func printSlice[T int | string](items []T) {
+	for _, item := range items {
+		fmt.Println(item)
+	}
+}
+
+type ApiResponse[T interface{}] struct {
+	status  int
+	data    T
+	message string
+	err     error
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 5, 5, 5}
+	str := []string{"chai", "elychi"}
+
+	printSlice(nums)
+	printSlice(str)
+}
