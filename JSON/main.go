3076ef31b9befd2d83d9979d5c360f2a5a8e8a48
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-11 22:04:18 +0530

    json encoder

--- JSON/main.go
+++ JSON/main.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type course struct {
+	Name     string   `json:"coursename"`
+	Price    int      `json:"courseprice"`
+	Platform string   `json:"website"`
+	password string   `json:"-"`
+	Tags     []string `json:"tags,omitempty"`
+}
+
 func main() {
-	
+	fmt.Println("welcome to JSON video")
+	EncodeJson()
+}
+
+func EncodeJson() {
+	lcoCourses := []course{
+		{
+			Name:     "Android Dev",
+			Price:    1000,
+			Platform: "LCO",
+			password: "1221",
+			Tags:     []string{"Android", "mobile dev"},
+		},
+		{
+			Name:     "IOS Dev",
+			Price:    1000,
+			Platform: "LCO",
+			password: "1221",
+			Tags:     []string{"IOS", "mobile dev"},
+		},
+		{
+			Name:     "WEB Dev",
+			Price:    1000,
+			Platform: "LCO",
+			password: "1221",
+			Tags:     nil,
+		},
+	}
+	// finalJSON, err := json.Marshal(lcoCourses)
+	finalJSON, err := json.MarshalIndent(lcoCourses, "", "\t")
+
+	// var jsonObject map[string]any
+	// json.Unmarshal(finalJSON, &jsonObject)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(finalJSON))
+
 }
