374d5879ab8f3cf45ba9bcc0dc3441852a6a30a2
kishanghosh090 <kishanghosh090@gmail.com>
2026-05-15 20:18:55 +0530

    add goroutine

--- goroutine/goroutine.go
+++ goroutine/goroutine.go
@@ -1,25 +1,44 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"sync"
+// )
+
+// func task(id int, w *sync.WaitGroup) {
+// 	defer w.Done()
+// 	fmt.Println("doing task", id)
+// }
+
+// func main() {
+// 	var wg sync.WaitGroup
+
+// 	for i := 0; i <= 100; i++ {
+// 		wg.Add(1)
+// 		go task(i, &wg)
+// 	}
+// 	// time.Sleep(time.Second * 2)
+
+// 	// wait groups goroutine
+// 	wg.Wait()
+
+// }
+
 package main
 
 import (
 	"fmt"
-	"sync"
+	"time"
 )
 
-func task(id int, w *sync.WaitGroup) {
-	defer w.Done()
-	fmt.Println("doing task", id)
+func main() {
+	go greeter("hello")
+	go greeter("world")
+	time.Sleep(time.Second * 1)
 }
 
-func main() {
-	var wg sync.WaitGroup
-
-	for i := 0; i <= 100; i++ {
-		wg.Add(1)
-		go task(i, &wg)
+func greeter(s string) {
+	for i := 0; i < 100; i++ {
+		fmt.Println(s)
 	}
-	// time.Sleep(time.Second * 2)
-
-	// wait groups goroutine
-	wg.Wait()
-
 }
